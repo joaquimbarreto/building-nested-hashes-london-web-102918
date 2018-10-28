12ca057438311f0008d44a55b57f0e606b38f1a4
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 19:30:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,11 @@
 def first_challenge
    epic_tragedy = {
+      :montague => {},
+     :capulet => {}
+end
+
+def fourth_challange
+  epic_tragedy = {
       :montague => {
         :patriarch => {name: "Lord Montague", age: "53"},
         :matriarch => {name: "Lady Montague", age: "54"},
